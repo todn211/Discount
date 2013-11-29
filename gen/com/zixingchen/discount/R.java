040e9acc0691954437d3d1c7eaa501172238a45d
todn211 <todn211@126.com>
2013-11-29 16:02:36 +0800

    test

--- gen/com/zixingchen/discount/R.java
+++ gen/com/zixingchen/discount/R.java
@@ -55,17 +55,20 @@ containing a value of this type.
     }
     public static final class drawable {
         public static final int arrow_right=0x7f020000;
-        public static final int corners_button=0x7f020001;
+        public static final int corners_button_selector=0x7f020001;
         public static final int goods_selected_bg=0x7f020002;
         public static final int ic_launcher=0x7f020003;
         public static final int lv_close=0x7f020004;
         public static final int lv_group_arrow=0x7f020005;
         public static final int lv_open=0x7f020006;
         public static final int main_bg=0x7f020007;
-        public static final int toolbar_bg=0x7f020008;
-        public static final int toolbar_button_disabled=0x7f020009;
-        public static final int toolbar_button_normar=0x7f02000a;
-        public static final int toolbar_button_pressed=0x7f02000b;
+        public static final int toolbar_back_button_selector=0x7f020008;
+        public static final int toolbar_back_normar=0x7f020009;
+        public static final int toolbar_back_pressed=0x7f02000a;
+        public static final int toolbar_bg=0x7f02000b;
+        public static final int toolbar_button_disabled=0x7f02000c;
+        public static final int toolbar_button_normar=0x7f02000d;
+        public static final int toolbar_button_pressed=0x7f02000e;
     }
     public static final class id {
         public static final int action_settings=0x7f09000c;
@@ -122,12 +125,17 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070003;
+        public static final int AppBaseTheme=0x7f070004;
         /**  Application theme. 
          */
         public static final int AppTheme=0x7f070000;
+        /**  圆角按钮 
+         */
         public static final int cornersButton=0x7f070001;
-        public static final int noAnimation=0x7f070002;
+        public static final int noAnimation=0x7f070003;
+        /**  工具条向左导航按钮 
+         */
+        public static final int toolbarBackButton=0x7f070002;
     }
     public static final class styleable {
         /**  公共属性 
