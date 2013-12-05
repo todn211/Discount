0526da1d164a293687ca6e7023cb7b0568882a3d
todn211 <todn211@126.com>
2013-12-05 11:16:58 +0800

    t

---
+++ src/com/zixingchen/discount/activity/GoodsDeailActivity.java
@@ -0,0 +1,18 @@
+package com.zixingchen.discount.activity;
+
+import com.zixingchen.discount.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * 商品详细信息展示页面
+ * @author 陈梓星
+ */
+public class GoodsDeailActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.goods_detail_activity);
+	}
+}
