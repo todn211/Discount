1d2ee5f88c335d5dcd53f69f8757a5578cfeeb82
todn211 <todn211@126.com>
2013-12-23 15:19:38 +0800

    test

--- gen/com/zixingchen/discount/R.java
+++ gen/com/zixingchen/discount/R.java
@@ -50,67 +50,87 @@ containing a value of this type.
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f050000;
-        public static final int activity_vertical_margin=0x7f050001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
-        public static final int arrow_right=0x7f020000;
-        public static final int corners_button_selector=0x7f020001;
-        public static final int goods_selected_bg=0x7f020002;
-        public static final int ic_launcher=0x7f020003;
-        public static final int lv_close=0x7f020004;
-        public static final int lv_group_arrow=0x7f020005;
-        public static final int lv_open=0x7f020006;
-        public static final int main_bg=0x7f020007;
-        public static final int toolbar_back_button_selector=0x7f020008;
-        public static final int toolbar_back_normar=0x7f020009;
-        public static final int toolbar_back_pressed=0x7f02000a;
-        public static final int toolbar_bg=0x7f02000b;
-        public static final int toolbar_button_disabled=0x7f02000c;
-        public static final int toolbar_button_normar=0x7f02000d;
-        public static final int toolbar_button_pressed=0x7f02000e;
+        public static final int add_green_icon=0x7f020000;
+        public static final int add_icon=0x7f020001;
+        public static final int arrow_right=0x7f020002;
+        public static final int back_icon=0x7f020003;
+        public static final int button_orange_normar=0x7f020004;
+        public static final int button_orange_pressed=0x7f020005;
+        public static final int corners_button_orange_selector=0x7f020006;
+        public static final int corners_button_selector=0x7f020007;
+        public static final int goods_type_bg=0x7f020008;
+        public static final int home_icon=0x7f020009;
+        public static final int ic_launcher=0x7f02000a;
+        public static final int lv_close=0x7f02000b;
+        public static final int lv_group_arrow=0x7f02000c;
+        public static final int lv_open=0x7f02000d;
+        public static final int main_bg=0x7f02000e;
+        public static final int refresh_icon=0x7f02000f;
+        public static final int toolbar_bg=0x7f020010;
+        public static final int toolbar_button_disabled=0x7f020011;
+        public static final int toolbar_button_normar=0x7f020012;
+        public static final int toolbar_button_pressed=0x7f020013;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000c;
-        public static final int btAdd=0x7f09000a;
-        public static final int btHome=0x7f090003;
-        public static final int btLeft=0x7f090001;
-        public static final int btRefresh=0x7f090009;
-        public static final int goodsSelectedContainer=0x7f090000;
-        public static final int ivIcon=0x7f090006;
-        public static final int lvGoodsSelected=0x7f090004;
-        public static final int lvMyAttention=0x7f09000b;
-        public static final int tvCurrentPrice=0x7f090008;
-        public static final int tvName=0x7f090005;
-        public static final int tvPrePrice=0x7f090007;
-        public static final int tvTitle=0x7f090002;
+        public static final int action_settings=0x7f0a0012;
+        public static final int btAdd=0x7f0a0010;
+        public static final int btBack=0x7f0a0000;
+        public static final int btFocusGoods=0x7f0a0001;
+        public static final int btHome=0x7f0a0005;
+        public static final int btLeft=0x7f0a0003;
+        public static final int btRefresh=0x7f0a000f;
+        public static final int goodsSelectedContainer=0x7f0a0007;
+        public static final int ivGoodsIcon=0x7f0a0009;
+        public static final int ivIcon=0x7f0a000c;
+        public static final int lvGoodsItem=0x7f0a0006;
+        public static final int lvGoodsType=0x7f0a0008;
+        public static final int lvMyAttention=0x7f0a0011;
+        public static final int tvCurrentPrice=0x7f0a000e;
+        public static final int tvName=0x7f0a000a;
+        public static final int tvPrePrice=0x7f0a000d;
+        public static final int tvPrice=0x7f0a000b;
+        public static final int tvTitle=0x7f0a0004;
+        public static final int wvGoodsDetail=0x7f0a0002;
     }
     public static final class layout {
-        public static final int goods_selected_activity=0x7f030000;
-        public static final int lv_goods_selected_item=0x7f030001;
-        public static final int lv_my_attention_child=0x7f030002;
-        public static final int lv_my_attention_group=0x7f030003;
-        public static final int main_activity=0x7f030004;
+        public static final int goods_detail_activity=0x7f030000;
+        public static final int goods_item_activity=0x7f030001;
+        public static final int goods_type_activity=0x7f030002;
+        public static final int lv_goods_item=0x7f030003;
+        public static final int lv_goods_type_item=0x7f030004;
+        public static final int lv_my_attention_child=0x7f030005;
+        public static final int lv_my_attention_group=0x7f030006;
+        public static final int main_activity=0x7f030007;
     }
     public static final class menu {
-        public static final int goods_selected=0x7f080000;
-        public static final int menu_main_activity=0x7f080001;
-        public static final int my_attention=0x7f080002;
+        public static final int goods=0x7f090000;
+        public static final int goods_selected=0x7f090001;
+        public static final int menu_main_activity=0x7f090002;
+        public static final int my_attention=0x7f090003;
     }
     public static final class string {
-        public static final int action_settings=0x7f060009;
-        public static final int addAttention=0x7f060005;
-        public static final int allGoodsType=0x7f060002;
-        public static final int app_name=0x7f060000;
-        public static final int close=0x7f06000b;
-        public static final int hello_world=0x7f06000a;
-        public static final int home=0x7f06000c;
-        public static final int imgContentDescript=0x7f060004;
-        public static final int more=0x7f060007;
-        public static final int myAttention=0x7f060001;
-        public static final int refresh=0x7f060006;
-        public static final int systemSetting=0x7f060003;
-        public static final int title_activity_goods_selected=0x7f060008;
+        public static final int action_settings=0x7f070009;
+        public static final int add=0x7f070005;
+        public static final int allGoodsType=0x7f070002;
+        public static final int app_name=0x7f070000;
+        public static final int back=0x7f07000e;
+        public static final int close=0x7f07000b;
+        public static final int failure=0x7f070011;
+        public static final int focus=0x7f07000f;
+        public static final int hello_world=0x7f07000a;
+        public static final int home=0x7f07000c;
+        public static final int imgContentDescript=0x7f070004;
+        public static final int more=0x7f070007;
+        public static final int myAttention=0x7f070001;
+        public static final int refresh=0x7f070006;
+        public static final int success=0x7f070010;
+        public static final int systemSetting=0x7f070003;
+        public static final int title_activity_goods=0x7f07000d;
+        public static final int title_activity_goods_selected=0x7f070008;
     }
     public static final class style {
         /** 
@@ -125,17 +145,21 @@ containing a value of this type.
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f070004;
+        public static final int AppBaseTheme=0x7f080004;
         /**  Application theme. 
          */
-        public static final int AppTheme=0x7f070000;
-        /**  圆角按钮 
+        public static final int AppTheme=0x7f080000;
+        /**  工具条圆角按钮 
          */
-        public static final int cornersButton=0x7f070001;
-        public static final int noAnimation=0x7f070003;
-        /**  工具条向左导航按钮 
+        public static final int cornersButton=0x7f080001;
+        /**  圆角按钮橙色 
          */
-        public static final int toolbarBackButton=0x7f070002;
+        public static final int cornersButtonOrange=0x7f080002;
+        public static final int noAnimation=0x7f080003;
+    }
+    public static final class xml {
+        public static final int focus_goods=0x7f050000;
+        public static final int table_goods_type=0x7f050001;
     }
     public static final class styleable {
         /**  公共属性 
