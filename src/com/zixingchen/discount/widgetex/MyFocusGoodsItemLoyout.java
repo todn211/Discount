a5ebb185def358c9cc703f35857e4516aa03395e
todn211 <todn211@126.com>
2013-12-27 18:08:16 +0800

    test

--- src/com/zixingchen/discount/widgetex/MyFocusGoodsItemLoyout.java
+++ src/com/zixingchen/discount/widgetex/MyFocusGoodsItemLoyout.java
@@ -1,5 +1,7 @@
 package com.zixingchen.discount.widgetex;
 
+import com.zixingchen.discount.R;
+
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
@@ -12,13 +14,21 @@ public class MyFocusGoodsItemLoyout extends RelativeLayout {
 
 	private GestureDetector gestureDetector;
 	private Context context;
-	private boolean isInterceptEevntToChild;//是否拦截事件传递到子控件
+	private boolean isInterceptEevntToChild = true;//是否拦截事件传递到子控件
 	private View moveTarget;//位于顶层的，需要向左移动的容器
+	private int btDeleteWidth;//删除按键的宽度
 
 	public MyFocusGoodsItemLoyout(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		this.context = context;
 		this.setLongClickable(true);
+	}
+	
+	@Override
+	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+		super.onSizeChanged(w, h, oldw, oldh);
+		
+		btDeleteWidth = this.findViewById(R.id.btDelete).getMeasuredWidth();
 	}
 	
 	@Override
@@ -31,9 +41,12 @@ public class MyFocusGoodsItemLoyout extends RelativeLayout {
 			@SuppressLint("NewApi")
 			public boolean onTouch(View v, MotionEvent event) {
 				if(MotionEvent.ACTION_UP == event.getAction()){
-					float currentX = moveTarget.getX();
-					if(currentX != -50)
+					if(moveTarget.getX() != -btDeleteWidth){
 						moveTarget.setX(0);
+						isInterceptEevntToChild = true;
+					}else{
+						isInterceptEevntToChild = false;
+					}
 				}
 				return gestureDetector.onTouchEvent(event);
 			}
@@ -57,8 +70,8 @@ public class MyFocusGoodsItemLoyout extends RelativeLayout {
 			float targetX = moveTarget.getX() - distanceX;
 			if(targetX > 0)
 				targetX = 0f;
-			else if(targetX < -50)
-				targetX = -50f;
+			else if(targetX < -btDeleteWidth)
+				targetX = -btDeleteWidth;
 			moveTarget.setX(targetX);
 			
 			//去掉点击产生的背景色
